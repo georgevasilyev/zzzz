bf15c4b77524a1a7f028714951c81111f30157c9
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-04 14:43:44 +0300

    Update blockmesh_install.sh

--- blockmesh_install.sh
+++ blockmesh_install.sh
@@ -1,5 +1,6 @@
 # wget --no-cache -q -O blockmesh.sh https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/blockmesh.sh && sudo chmod +x blockmesh.sh && ./blockmesh.sh
-mkdir /root/blockmesh && cd /root/blockmesh
+cd
+cd ..
 wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.331/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 tar -xzvf blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 sleep 1
@@ -9,14 +10,14 @@ screen -S blockmesh
 # /root/blockmesh/target/x86_64-unknown-linux-gnu/release/blockmesh-cli --email gera.122@mail.ru --password Emzige8672$
 
 
-sudo bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
+bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
 [Unit]
 Description=BlockMesh CLI Service
 After=network.target
 
 [Service]
 User=$USERNAME
-ExecStart=/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email gera.122@mail.ru --password Emzige8672$
+ExecStart=/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email 9179122122@mail.ru --password Emzige8672$
 WorkingDirectory=/target/x86_64-unknown-linux-gnu/release
 Restart=on-failure
 
@@ -29,3 +30,4 @@ sudo systemctl enable blockmesh
 sudo systemctl restart blockmesh
 
 sudo journalctl -u blockmesh -f
+sudo journalctl -u blockmesh -n 30 -f
