eb746ef35b6cc58029c2ead6ef9a3c07ab49fe8a
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-04 13:40:18 +0300

    Update blockmesh_install.sh

--- blockmesh_install.sh
+++ blockmesh_install.sh
@@ -1,1 +1,9 @@
-wget --no-cache -q -O blockmesh.sh https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/blockmesh.sh && sudo chmod +x blockmesh.sh && ./blockmesh.sh
+# wget --no-cache -q -O blockmesh.sh https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/blockmesh.sh && sudo chmod +x blockmesh.sh && ./blockmesh.sh
+mkdir /root/blockmesh && cd /root/blockmesh
+wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.331/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
+tar -xzvf blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
+sleep 1
+rm blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
+cd target/x86_64-unknown-linux-gnu/release/
+
+# /root/blockmesh/target/x86_64-unknown-linux-gnu/release/blockmesh-cli --email gera.122@mail.ru --password Emzige8672$
