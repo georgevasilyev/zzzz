a987be8174a9349baa108d323fe5b89c740bde57
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-04 14:15:23 +0300

    Update blockmesh_install.sh

--- blockmesh_install.sh
+++ blockmesh_install.sh
@@ -7,3 +7,25 @@ rm blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 cd target/x86_64-unknown-linux-gnu/release/
 screen -S blockmesh 
 # /root/blockmesh/target/x86_64-unknown-linux-gnu/release/blockmesh-cli --email gera.122@mail.ru --password Emzige8672$
+
+
+sudo bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
+[Unit]
+Description=BlockMesh CLI Service
+After=network.target
+
+[Service]
+User=$USERNAME
+ExecStart=/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email gera.122@mail.ru --password Emzige8672$
+WorkingDirectory=/target/x86_64-unknown-linux-gnu/release
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOT"
+
+sudo systemctl daemon-reload
+sudo systemctl enable blockmesh
+sudo systemctl restart blockmesh
+
+sudo journalctl -u blockmesh -f
