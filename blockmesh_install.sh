75485a4858a1373b111c04a119683d4ea7b48592
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-05 11:14:45 +0300

    Update blockmesh_install.sh

--- blockmesh_install.sh
+++ blockmesh_install.sh
@@ -1,12 +1,11 @@
 # wget --no-cache -q -O blockmesh.sh https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/blockmesh.sh && sudo chmod +x blockmesh.sh && ./blockmesh.sh
-cd
-cd ..
+cd /
 wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.331/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 tar -xzvf blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 sleep 1
 rm blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 cd target/x86_64-unknown-linux-gnu/release/
-screen -S blockmesh 
+# screen -S blockmesh 
 # /root/blockmesh/target/x86_64-unknown-linux-gnu/release/blockmesh-cli --email gera.122@mail.ru --password Emzige8672$
 
 
@@ -25,9 +24,9 @@ Restart=always
 WantedBy=multi-user.target
 EOT"
 
-sudo systemctl daemon-reload
-sudo systemctl enable blockmesh
-sudo systemctl restart blockmesh
+systemctl daemon-reload
+systemctl enable blockmesh
+systemctl restart blockmesh
 
-sudo journalctl -u blockmesh -f
-sudo journalctl -u blockmesh -n 30
+journalctl -u blockmesh -f
+journalctl -u blockmesh -n 30
