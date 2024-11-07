9a4b3a035ec445fa54cc328f1372e108f22e30bb
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-07 18:01:28 +0300

    Update blockmesh_update.sh

--- blockmesh_update.sh
+++ blockmesh_update.sh
@@ -20,6 +20,11 @@ rm blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 cd target/x86_64-unknown-linux-gnu/release/
 # /root/blockmesh/target/x86_64-unknown-linux-gnu/release/blockmesh-cli --email gera.122@mail.ru --password Emzige8672$
 
+# gera.122@mail.ru                    FAR-2060-1
+# 9179122122@mail.ru                  FAR-2060-2
+# paulmartin0185097@gmail.com          4090-1
+# donaldgonzalez5355887@gmail.com      4090-2
+# oooavers2018@mail.ru                GRA-2060-5
 
 bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
 [Unit]
@@ -28,7 +33,7 @@ After=network.target
 
 [Service]
 User=root
-ExecStart=/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email oooavers2018@mail.ru --password Emzige8672$
+ExecStart=/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email gera.122@mail.ru --password Emzige8672$
 WorkingDirectory=/target/x86_64-unknown-linux-gnu/release
 Restart=always
 
