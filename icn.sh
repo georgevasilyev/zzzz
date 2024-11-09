b943eb2ff9fa31643c2f13aa8016efe5792d4173
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-09 21:47:15 +0300

    Update icn.sh

--- icn.sh
+++ icn.sh
@@ -11,7 +11,7 @@
 sudo apt install screen -y
 # screen -S icn && curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p badcb80e6a16145619b0532affe3b66e52418c28bff8be3aabb16d83eb8162cc
 # screen -dmS icn bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p badcb80e6a16145619b0532affe3b66e52418c28bff8be3aabb16d83eb8162cc"
-cd / && screen -dmS icn bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p 0992d42cf51a5a8b4fe8ebd64af80dd3eb7dc5ddb4991712592adfbf8aa277cf > icn.log 2>&1"
+cd / && screen -dmS icn bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p 70048c2f1038ab1ebf94b0697eabab06283f0afb4a98db708c85d7646010ab5b > icn.log 2>&1"
 tail -n 20 /icn.log
 
 
