3964462dbe2ac350ab5b49fd6bce6b0b8360cbf5
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-10 22:32:57 +0300

    Update blockmesh_update.sh

--- blockmesh_update.sh
+++ blockmesh_update.sh
@@ -4,10 +4,10 @@
 # wget https://raw.githubusercontent.com/georgevasilyev/zzzz/refs/heads/main/blockmesh_update.sh && sudo chmod +x blockmesh_update.sh && ./blockmesh_update.sh
 # journalctl -u blockmesh --no-hostname -o cat -n 30
 sudo systemctl stop blockmesh
-sudo systemctl disable blockmesh
-sudo rm /etc/systemd/system/blockmesh.service
-sudo systemctl daemon-reload
-sleep 1
+##sudo systemctl disable blockmesh
+##sudo rm /etc/systemd/system/blockmesh.service
+##sudo systemctl daemon-reload
+##sleep 1
 rm -rf /target
 sleep 1
 
@@ -19,7 +19,7 @@ wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.35
 tar -xzvf blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
 sleep 1
 rm blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
-cd target/x86_64-unknown-linux-gnu/release/
+##cd target/x86_64-unknown-linux-gnu/release/
 # /root/blockmesh/target/x86_64-unknown-linux-gnu/release/blockmesh-cli --email gera.122@mail.ru --password Emzige8672$
 
 # gera.122@mail.ru                    FAR-2060-1
@@ -28,25 +28,25 @@ cd target/x86_64-unknown-linux-gnu/release/
 # donaldgonzalez5355887@gmail.com      4090-2
 # oooavers2018@mail.ru                GRA-2060-5
 
-bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
-[Unit]
-Description=BlockMesh CLI Service
-After=network.target
+##bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
+##[Unit]
+##Description=BlockMesh CLI Service
+##After=network.target
 
-[Service]
-User=root
-ExecStart=/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email oooavers2018@mail.ru --password Emzige8672$
-WorkingDirectory=/target/x86_64-unknown-linux-gnu/release
-Restart=always
+##[Service]
+##User=root
+##ExecStart=/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email oooavers2018@mail.ru --password Emzige8672$
+##WorkingDirectory=/target/x86_64-unknown-linux-gnu/release
+##Restart=always
 
-[Install]
-WantedBy=multi-user.target
-EOT"
+##[Install]
+##WantedBy=multi-user.target
+##EOT"
 
 
 systemctl daemon-reload
 sleep 2
-systemctl enable blockmesh
+##systemctl enable blockmesh
 systemctl restart blockmesh
 journalctl -u blockmesh -f
 
