d68ff6ae26c8533d13d4c227cfae3ba5ea458824
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-12 18:40:17 +0300

    Update fizz_install.sh

--- fizz_install.sh
+++ fizz_install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-# install docker and everything needed
+# install everything needed
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# install docker
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo $VERSION_CODENAME) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+chmod +x /root/fizzup-v1.1.0.sh
+./fizzup-v1.1.0.sh
+
+# To fetch the logs, run:
+docker-compose -f ~/.spheron/fizz/docker-compose.yml logs -f
+
+# To stop the service, run:
+docker-compose -f ~/.spheron/fizz/docker-compose.yml down
+
+# github
+# mm
