4ba1ff827c735ee520359c35446b58ff60f585f2
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-15 17:09:39 +0300

    Create nillion_install.sh

---
+++ nillion_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# может и не надо
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+# Устанавливаем Accuser Image
+docker pull nillion/verifier:v1.0.1
+
+# Создаем директорию для Accuser
+mkdir -p /root/nillion/verifier
+
+# Запускаем контейнер для инициализации Accuser и Регистрации
+docker run -v /root/nillion/verifier:/var/tmp nillion/verifier:v1.0.1 initialise
+
+# Копируем Verifier account id
+
+# https://faucet.testnet.nillion.com/
+
+cat /root/nillion/verifier/credentials.json
+# копируем приватник
+
+# идем на сайт и привязываем кеплр
+# https://verifier.nillion.com/verifier
+
+# Далее запускаем ноду
+docker run -v /root//nillion/verifier:/var/tmp nillion/verifier:v1.0.1 verify --rpc-endpoint "https://testnet-nillion-rpc.lavenderfive.com" 
