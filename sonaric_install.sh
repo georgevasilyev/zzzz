9ea09997a45ab669b5a5220baa905d7ff1dfa0d7
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-15 15:33:06 +0300

    Update sonaric_install.sh

--- sonaric_install.sh
+++ sonaric_install.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+# lsb_release -a
+
+sudo apt install curl git jq build-essential gcc unzip wget lz4 -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+. /etc/os-release
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+sudo apt-get -y install buildah
+``
+
+sh -c "$(curl -fsSL http://get.sonaric.xyz/scripts/install.sh)"
+
+
+# sonaric points
+
+
