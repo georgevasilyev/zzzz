ab65e45d45013f53ae0382510e3ba8b91e3d4c29
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2024-11-21 10:20:28 +0300

    Update icn.sh

--- icn.sh
+++ icn.sh
@@ -8,12 +8,15 @@
 # 2-3070-4    102       0xb6a0fadc5a197a6f34601e9524832804c988258dd1b545552f9452b337add5f6
 # 2-3070-5    104       0xae06ce52e0c5432327effd3fbbc4e6774e8a50c24880ca92c23984b6431e5e8b
 # 2-3070-6    106       0x16b5f7cfb754b630ecbee08aeab1541e5acd667b94b04753fd9eb297a33f2d0d
+# 3-3070-1    108       0xe3745215ebaf7b9dc1f7588fe4ed8e1809822381f9b4f1d2061c5664df2649f5
+# 3-3070-2    110       0x43c6c5b6ab0f43eb22901ff5a03c54d8f6bb5a0ea04579ea4082ec87cd0cec8f
+
 
 #!/bin/bash
 sudo apt install screen -y
 # screen -S icn && curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p badcb80e6a16145619b0532affe3b66e52418c28bff8be3aabb16d83eb8162cc
 # screen -dmS icn bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p badcb80e6a16145619b0532affe3b66e52418c28bff8be3aabb16d83eb8162cc"
-cd / && screen -dmS icn bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p 0x16b5f7cfb754b630ecbee08aeab1541e5acd667b94b04753fd9eb297a33f2d0d > icn.log 2>&1"
+cd / && screen -dmS icn bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p d107498a19082333bbec96595704156f4562da56857ae6eab99c02492f1fae30 > icn.log 2>&1"
 tail -n 20 /icn.log
 tail -f /icn.log
 
