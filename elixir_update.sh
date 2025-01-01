6309f1e8a40345b5c5a6600f6611838fc39bc594
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2025-01-02 00:01:35 +0300

    Update elixir_update.sh

--- elixir_update.sh
+++ elixir_update.sh
@@ -1,7 +1,8 @@
+# testnet update command 
 docker kill elixir
 docker rm elixir
 docker pull elixirprotocol/validator --platform linux/amd64
 docker run --env-file <path to env file> --platform linux/amd64 --name elixir -p 17690:17690 elixirprotocol/validator
 
-
+# hiveOS update command
 docker kill elixir-mainnet && docker rm elixir-mainnet && docker pull elixirprotocol/validator --platform linux/amd64 && docker run --name elixir-mainnet --env-file /root/validator.env --platform linux/amd64 -p 17690:17690 --restart unless-stopped elixirprotocol/validator
