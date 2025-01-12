cc3f9bb90a2c0b34dc243f7e9d82785af12cdf88
georgevasilyev <100374459+georgevasilyev@users.noreply.github.com>
2025-01-12 18:46:21 +0300

    Update xechain.sh

--- xechain.sh
+++ xechain.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 #wget https://raw.githubusercontent.com/georgevasilyev/zzzz/refs/heads/main/xechain.sh && chmod +x xechain.sh && ./xechain.sh
 
-#cd ~
-#sudo apt remove golang-go -y
-#sudo rm -rf /usr/local/go
-#apt update -y
-#wget https://go.dev/dl/go1.21.3.linux-amd64.tar.gz
-#sudo tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
-#echo -e "\nexport PATH=\$PATH:/usr/local/go/bin\nexport GOPATH=\$HOME/goproject\nexport PATH=\$PATH:\$GOPATH/bin" >> ~/.profile
-#source ~/.profile
-#mkdir -p $HOME/goproject
-#apt install git make build-essential -y
-#go version
+cd ~
+sudo apt remove golang-go -y
+sudo rm -rf /usr/local/go
+apt update -y
+wget https://go.dev/dl/go1.21.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
+echo -e "\nexport PATH=\$PATH:/usr/local/go/bin\nexport GOPATH=\$HOME/goproject\nexport PATH=\$PATH:\$GOPATH/bin" >> ~/.profile
+source ~/.profile
+mkdir -p $HOME/goproject
+apt install git make build-essential -y
+go version
 
 #install miner
-mkdir -p /root/xechain/miner
-cd /root/xechain/miner
-wget https://github.com/XeChain/xeMiner/releases/download/v1.0/linux.zip
-unzip linux.zip -d /root/xechain/miner
-rm linux.zip
-chmod +x ./xeMiner
+#mkdir -p /root/xechain/miner
+#cd /root/xechain/miner
+#wget https://github.com/XeChain/xeMiner/releases/download/v1.0/linux.zip
+#unzip linux.zip -d /root/xechain/miner
+#rm linux.zip
+#chmod +x ./xeMiner
 #performance testing
 #./xeMiner -mock -all
 #Show OpenCL devices informations
 #./xeMiner -info -all
 
 #install node
-cd /root/xechain
-git clone https://github.com/XeChain/xe-core
+#cd /root/xechain
+#git clone https://github.com/XeChain/xe-core
 #cd xe-core
 #make xe
 
